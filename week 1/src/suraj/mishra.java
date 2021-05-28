8a71be4907550551253b9b7f982d1366434bcc1e
Suraj Mishra <mishrasur7@gmail.com>
2021-05-28 23:58:15 +0300

    java starting

---
+++ week 1/src/suraj/mishra.java
@@ -0,0 +1,19 @@
+package suraj;
+
+import java.util.Iterator;
+
+public class mishra {
+
+	public static void main(String[] args) {
+		System.out.println("Hello"); 
+		System.out.println(367); 
+		System.out.println("suraj");
+		System.out.println();
+		
+
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
